518d121639d7fbf857a8cd08d23867de1ebb6cca
touy <touya.ra@gmail.com>
2019-12-18 23:43:30 +0700

    update
    Signed-off-by: touy <touya.ra@gmail.com>

--- ts/routes/router.ts
+++ ts/routes/router.ts
@@ -8,7 +8,7 @@ export class Router {
                 message: 'GET request successfulll!!!!'
             })
         }).post((req: Request, res: Response) => {    
-            const body = req.body.Body;        
+            const body = req.body;        
             console.log(body);
             res.status(200).send({
                 message: 'GET request successfulll!!!!'
